2e019686928122fe1b9b699bf1fde91d48c40a7f
Alitarek3102005 <tali19985@gmail.com>
2026-08-16 21:56:01 +0300

    Adding Category Entity

---
+++ src/main/java/com/example/bookstore/controller/CategoryController.java
@@ -0,0 +1,56 @@
+package com.example.bookstore.controller;
+
+import com.example.bookstore.api.CategoriesApi;
+import com.example.bookstore.dto.BookResponse;
+import com.example.bookstore.dto.CategoryPatchRequest;
+import com.example.bookstore.dto.CategoryRequest;
+import com.example.bookstore.dto.CategoryResponse;
+import com.example.bookstore.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+public class CategoryController implements CategoriesApi {
+    private final CategoryService categoryService;
+
+    @Override
+    public ResponseEntity<CategoryResponse> createCategory( CategoryRequest categoryRequest) {
+        return new ResponseEntity<>(categoryService.save(categoryRequest), HttpStatus.CREATED);
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteCategory(UUID categoryId) {
+        categoryService.delete(categoryId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @Override
+    public ResponseEntity<List<CategoryResponse>> getAllCategories() {
+        return ResponseEntity.ok(categoryService.findAll());
+    }
+
+    @Override
+    public ResponseEntity<CategoryResponse> getCategoryById(UUID categoryId) {
+        return new ResponseEntity<>(categoryService.findById(categoryId), HttpStatus.OK);
+    }
+    @Override
+    public ResponseEntity<List<BookResponse>> getBooksByCategory(UUID categoryId) {
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public ResponseEntity<CategoryResponse> patchCategory(UUID categoryId, CategoryPatchRequest categoryPatchRequest) {
+        return new ResponseEntity<>(categoryService.patch(categoryId, categoryPatchRequest), HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<CategoryResponse> updateCategory(UUID categoryId, CategoryRequest categoryRequest) {
+        return new ResponseEntity<>(categoryService.update(categoryRequest, categoryId), HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/example/bookstore/service/CategoryService.java
@@ -0,0 +1,68 @@
+package com.example.bookstore.service;
+
+import com.example.bookstore.domain.Category;
+import com.example.bookstore.dto.CategoryPatchRequest;
+import com.example.bookstore.dto.CategoryRequest;
+import com.example.bookstore.dto.CategoryResponse;
+import com.example.bookstore.exception.customExceptions.CategoryNotFoundException;
+import com.example.bookstore.mapper.CategoryMapper;
+import com.example.bookstore.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+    private final CategoryMapper categoryMapper;
+
+    public List<CategoryResponse> findAll() {
+        Iterable<Category> categories=categoryRepository.findAll();
+        List<CategoryResponse> categoriesResponse=new ArrayList<>();
+        for (Category category : categories) {
+            categoriesResponse.add(categoryMapper.toResponse(category));
+        }
+        return categoriesResponse;
+    }
+
+    public CategoryResponse findById(UUID id) {
+        Category category = categoryRepository.findById(id).orElse(null);
+        if (category!=null) {
+            return categoryMapper.toResponse(category);
+        }else {
+            throw new CategoryNotFoundException("Category not found");
+        }
+    }
+    public CategoryResponse save(CategoryRequest category) {
+        Category categoryEntity = categoryMapper.toEntity(category);
+        categoryRepository.save(categoryEntity);
+        return categoryMapper.toResponse(categoryEntity);
+    }
+    public CategoryResponse update(CategoryRequest category, UUID id) {
+        Category categoryEntity =categoryRepository.findById(id).orElse(null);
+        if (categoryEntity!=null) {
+            categoryEntity=categoryMapper.toEntity(category);
+            categoryRepository.save(categoryEntity);
+            return categoryMapper.toResponse(categoryEntity);
+        }else{
+            throw new CategoryNotFoundException("Category not found");
+        }
+    }
+    public void delete(UUID id) {
+        categoryRepository.deleteById(id);
+    }
+    public CategoryResponse patch(UUID id, CategoryPatchRequest categoryPatchRequest) {
+        Category categoryEntity = categoryRepository.findById(id).orElse(null);
+        if (categoryEntity!=null) {
+            categoryMapper.patchEntityFromRequest(categoryPatchRequest, categoryEntity);
+            categoryRepository.save(categoryEntity);
+            return categoryMapper.toResponse(categoryEntity);
+        }else{
+            throw new CategoryNotFoundException("Category not found");
+        }
+    }
+
+}
