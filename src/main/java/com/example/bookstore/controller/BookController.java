30c85e3680ea04912ef28ebf2994511f484fcfc5
TamerA2 <ahmed.tamer1@vodafone.com>
2026-08-19 00:37:55 +0300

    Book Domain

--- src/main/java/com/example/bookstore/controller/BookController.java
+++ src/main/java/com/example/bookstore/controller/BookController.java
@@ -1,4 +1,51 @@
 package com.example.bookstore.controller;
 
-public class BookController {
+import com.example.bookstore.api.BooksApi;
+import com.example.bookstore.dto.BookPatchRequest;
+import com.example.bookstore.dto.BookRequest;
+import com.example.bookstore.dto.BookResponse;
+import com.example.bookstore.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+public class BookController implements com.example.bookstore.api.BooksApi {
+    private final BookService bookService;
+    @Override
+    public ResponseEntity<BookResponse> createBook(BookRequest bookRequest) {
+        return new ResponseEntity<>(bookService.addBook(bookRequest),HttpStatus.CREATED);
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteBook(UUID bookId) {
+        bookService.deleteBook(bookId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @Override
+    public ResponseEntity<List<BookResponse>> getAllBooks() {
+        return new ResponseEntity<>(bookService.getAll(),HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<BookResponse> getBookById(UUID bookId) {
+        return new ResponseEntity<>(bookService.getById(bookId),HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<BookResponse> patchBook(UUID bookId, BookPatchRequest bookPatchRequest) {
+        return BooksApi.super.patchBook(bookId, bookPatchRequest);
+    }
+
+    @Override
+    public ResponseEntity<BookResponse> updateBook(UUID bookId, BookRequest bookRequest) {
+
+        return new ResponseEntity<>(bookService.updateBook(bookId,bookRequest),HttpStatus.OK);
+    }
 }
--- src/main/java/com/example/bookstore/repository/BookRepository.java
+++ src/main/java/com/example/bookstore/repository/BookRepository.java
@@ -1,4 +1,11 @@
 package com.example.bookstore.repository;
 
-public interface BookRepository {
+import com.example.bookstore.domain.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, UUID> {
 }
