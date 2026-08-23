f2f3f2b163da7074dc7d39bc989fa9777499e004
Alitarek3102005 <tali19985@gmail.com>
2026-08-23 14:51:40 +0300

    add BookStoreApplicationTests

--- src/test/java/com/example/bookstore/BookStoreApplicationTests.java
+++ src/test/java/com/example/bookstore/BookStoreApplicationTests.java
@@ -2,12 +2,18 @@ package com.example.bookstore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.flyway.enabled=false",
+        "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class BookStoreApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
